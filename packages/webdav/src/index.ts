f64de61cc4f00e4491b23de1b0c112feffbeb97b
XLor <yjl9903@vip.qq.com>
2023-09-15 16:12:42 +0800

    fix(webdav): type mismatch

--- packages/webdav/src/index.ts
+++ packages/webdav/src/index.ts
@@ -2,39 +2,39 @@ import { BreadFSProvider } from '@breadfs/core';
 import { ReadableStream, WritableStream } from 'stream/web';
 
 export class WebDAVFSProvider extends BreadFSProvider {
-  mkdir(path: string): Promise<string> {
+  public mkdir(path: string): Promise<void> {
     throw new Error('Method not implemented.');
   }
 
-  createReadStream(path: string): ReadableStream<any> {
+  public createReadStream(path: string): ReadableStream<any> {
     throw new Error('Method not implemented.');
   }
 
-  createWriteStream(path: string): WritableStream<any> {
+  public createWriteStream(path: string): WritableStream<any> {
     throw new Error('Method not implemented.');
   }
 
-  readFile(path: string): ReadableStream<any> {
+  public readFile(path: string): ReadableStream<any> {
     throw new Error('Method not implemented.');
   }
 
-  writeFile(path: string, stream: ReadableStream<any>): Promise<void> {
+  public writeFile(path: string, stream: ReadableStream<any>): Promise<void> {
     throw new Error('Method not implemented.');
   }
 
-  remove(path: string): Promise<void> {
+  public remove(path: string): Promise<void> {
     throw new Error('Method not implemented.');
   }
 
-  stat(path: string): Promise<{}> {
+  public stat(path: string): Promise<{}> {
     throw new Error('Method not implemented.');
   }
 
-  list(path: string): Promise<string> {
+  public list(path: string): Promise<string> {
     throw new Error('Method not implemented.');
   }
 
-  walk(path: string): AsyncIterable<{}> {
+  public walk(path: string): AsyncIterable<{}> {
     throw new Error('Method not implemented.');
   }
 }
