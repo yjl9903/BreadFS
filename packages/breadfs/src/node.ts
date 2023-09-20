7a1666b36aa0e1e2aab70a54dfa3f9da40f6a34c
XLor <yjl9903@vip.qq.com>
2023-09-21 02:47:55 +0800

    feat(breadfs): export fs

--- packages/breadfs/src/node.ts
+++ packages/breadfs/src/node.ts
@@ -1,3 +1,8 @@
+import { NodeFS } from '@breadfs/node';
+import { BreadFS } from '@breadfs/core';
+
+export const fs = BreadFS.of(NodeFS);
+
 export * from '@breadfs/core';
 
 export * from '@breadfs/node';
